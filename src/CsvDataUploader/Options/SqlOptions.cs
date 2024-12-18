a61dec9d0b9bd5feb89b1e9bbe453f24d6b5abba
MJ Schanne <mjschanne@microsoft.com>
2024-12-18 03:20:11 -0500

    Add updated options classes

---
+++ src/CsvDataUploader/Options/SqlOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace CsvDataUploader.Options;
+
+public class SqlOptions
+{
+
+    // can fully configure the connection string yourself
+    public string ConnectionString { get; set; } = string.Empty;
+
+    // required if not using preconfigured connection string
+    public string Endpoint { get; set; } = string.Empty;
+    public string DatabaseName { get; set; } = string.Empty;
+
+    // leave blank if you want to use managed identity
+    public string UserId { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+
+    public bool ClearDatabaseDataBeforeRun { get; set; } = false;
+}
